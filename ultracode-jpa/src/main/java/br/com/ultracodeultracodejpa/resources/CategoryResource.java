95bac0f01181928a0811fda56890e9fda9ccfe71
Arthur Souza <arthurr.souzaa98@gmail.com>
2020-04-20 22:25:59 -0300

    category domain created

--- ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/resources/CategoryResource.java
+++ ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/resources/CategoryResource.java
@@ -1,15 +1,31 @@
 package br.com.ultracodeultracodejpa.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.ultracodeultracodejpa.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET, value="/list")
-	public String Listar() {
-		return "Rest está funcionando";
+	public List<Category> Listar() {
+		
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> catList = new ArrayList<>();
+		
+		catList.add(cat1);
+		catList.add(cat2);
+		
+		return catList;
+		
+		
 	}
 }
