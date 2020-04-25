d13f50d46037a842c4e9ee58447829b5ca5eeb17
Arthur Souza <arthurr.souzaa98@gmail.com>
2020-04-24 21:54:43 -0300

    feat: categogry table initializing with pre-defined values

--- ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/UltracodeJpaApplication.java
+++ ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/UltracodeJpaApplication.java
@@ -1,13 +1,32 @@
 package br.com.ultracodeultracodejpa;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class UltracodeJpaApplication {
+import br.com.ultracodeultracodejpa.domain.Category;
+import br.com.ultracodeultracodejpa.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class UltracodeJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(UltracodeJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
