e6468f644692025ffad2a4b8eddf2e6a55066372
Arthur Souza <arthurr.souzaa98@gmail.com>
2020-06-24 20:31:43 -0300

    feat: implementing post category

--- ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/resources/CategoryResource.java
+++ ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/resources/CategoryResource.java
@@ -1,11 +1,15 @@
 package br.com.ultracodeultracodejpa.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.ultracodeultracodejpa.domain.Category;
 import br.com.ultracodeultracodejpa.services.CategoryService;
@@ -22,6 +26,13 @@ public class CategoryResource {
 		Category obj = service.getCategory(id);
 		
 		return ResponseEntity.ok().body(obj);
-		
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Category obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/services/CategoryService.java
+++ ultracode-jpa/src/main/java/br/com/ultracodeultracodejpa/services/CategoryService.java
@@ -21,4 +21,9 @@ public class CategoryService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado"));
 	}
 	
+	public Category insert(Category obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 }
